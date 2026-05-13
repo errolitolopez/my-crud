5e2ff84480904b2340500d9c08bdd2d291acfee4
Errol Lopez <errolitolopez@gmail.com>
2026-05-13 10:22:47 +0800

    Added gemma 4 - 31b model & added chat options thinking level to minimal

--- src/main/java/com/errolito/mycrud/controller/AiChatPageController.java
+++ src/main/java/com/errolito/mycrud/controller/AiChatPageController.java
@@ -13,9 +13,10 @@ public class AiChatPageController {
     @GetMapping("/spring-ai/chat")
     public String chat(Model model) {
         Map<String, String> llm = new LinkedHashMap<>() {{
+            put("Gemma 4 - 26b", "gemma-4-26b-a4b-it");
+            put("Gemma 4 - 31b", "gemma-4-31b-it");
             put("Gemini 3.1 Flash", "gemini-3.1-flash-lite-preview");
             put("Gemini 2.5 Flash", "gemini-2.5-flash-lite");
-            put("Gemma 4", "gemma-4-26b-a4b-it");
         }};
 
         model.addAttribute("models", llm);
